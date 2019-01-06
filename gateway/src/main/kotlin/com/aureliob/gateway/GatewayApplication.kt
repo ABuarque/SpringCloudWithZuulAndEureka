88009c604552c2b0909ea244c59e9bc1a158a70b
aureliosupercash <aurelio@super.cash>
2019-01-06 00:11:38 -0300

    added zuul configurations

--- gateway/src/main/kotlin/com/aureliob/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/aureliob/gateway/GatewayApplication.kt
@@ -3,7 +3,9 @@ package com.aureliob.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 class GatewayApplication
