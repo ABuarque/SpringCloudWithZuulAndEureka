b2b43f3176dd2ad49802cccad25a33fc6ee03957
aureliosupercash <aurelio@super.cash>
2019-01-08 15:36:13 -0300

    removed rest template with load balancing

--- api-client/src/main/kotlin/com/aureliob/apiclient/resources/ClientResource.kt
+++ api-client/src/main/kotlin/com/aureliob/apiclient/resources/ClientResource.kt
@@ -8,11 +8,18 @@ import org.springframework.web.client.RestTemplate
 @RequestMapping("/analysis")
 class ClientResource {
 
-    @Autowired
-    private lateinit var restTemplate: RestTemplate
+    //@Autowired
+    //private lateinit var restTemplate: RestTemplate
+    private val restTemplate = RestTemplate()
+
+    private companion object {
+        val SERVER_HOST = System.getenv("SERVER_HOST") ?: "localhost"
+        val SERVER_PORT = System.getenv("SERVER_PORT") ?: "8500"
+        val SERVER_ENDPOINT = "http://$SERVER_HOST:$SERVER_PORT"
+    }
 
     @RequestMapping("/{name}", method = [RequestMethod.GET])
     fun getUsersWithName(@PathVariable name: String) = restTemplate
-                .getForObject("http://api-server/users/$name",String::class.java)
+                .getForObject("$SERVER_ENDPOINT/users/$name", String::class.java)
 
 }
