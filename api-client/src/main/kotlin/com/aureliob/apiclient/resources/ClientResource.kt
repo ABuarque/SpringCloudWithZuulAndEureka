015ed67127bd20138ad36227540281dfe0b7a748
aureliosupercash <aurelio@super.cash>
2019-01-09 15:32:23 -0300

    small changer

--- api-client/src/main/kotlin/com/aureliob/apiclient/resources/ClientResource.kt
+++ api-client/src/main/kotlin/com/aureliob/apiclient/resources/ClientResource.kt
@@ -18,22 +18,24 @@ class ClientResource {
 
     private companion object {
         val SERVER_HOST = System.getenv("MY_SERVER_HOST") ?: "localhost"
-        val SERVER_PORT = System.getenv("MY_SERVER_PORT") ?: "8500"
-        val SERVER_ENDPOINT = "http://$SERVER_HOST:$SERVER_PORT"
+        val SERVER_PORT = System.getenv("MY_SERVER_PORT") ?: "vaca"
+        //val SERVER_ENDPOINT = "http://$SERVER_HOST:$SERVER_PORT"
+        val API_ENDPOINT = System.getenv("MY_VALUE") ?: "http://localhost:8500"
     }
 
     @RequestMapping(method = [RequestMethod.GET])
     fun online(): String {
-        println("SERVER_ENDPOINT: $SERVER_ENDPOINT")
+        //println("SERVER_ENDPOINT: $SERVER_ENDPOINT")
+        println("API_ENDPOINT: $API_ENDPOINT")
         println("VALUE: $my_value")
         return "online"
     }
 
     @RequestMapping("/{name}", method = [RequestMethod.GET])
     fun getUsersWithName(@PathVariable name: String): String? {
-        println("SERVER_ENDPOINT: $SERVER_ENDPOINT")
+        println("SERVER_ENDPOINT: $API_ENDPOINT")
         return restTemplate
-                .getForObject("$SERVER_ENDPOINT/users/$name", String::class.java)
+                .getForObject("$API_ENDPOINT/users/$name", String::class.java)
     }
 
 }
