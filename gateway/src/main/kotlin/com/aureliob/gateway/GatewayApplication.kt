41cc5f939b0618648a068fab7d596dec28dc56ba
aureliosupercash <aurelio@super.cash>
2019-01-09 15:31:59 -0300

    removed eureka server annotation

--- gateway/src/main/kotlin/com/aureliob/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/aureliob/gateway/GatewayApplication.kt
@@ -2,15 +2,17 @@ package com.aureliob.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @EnableZuulProxy
-@EnableEurekaServer
+//@EnableEurekaServer
 @SpringBootApplication
 class GatewayApplication
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
+    println("USERS: ${System.getenv("USER_SERVER_ENDPOINT")}")
+    println("BACKEND: ${System.getenv("ANALYSIS_SERVER_ENDPOINT")}")
 }
 
